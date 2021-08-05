800c620d6b2b72daa5b062ac7d841c4423a4aaf5
Flouse <flouse@gmail.com>
2021-08-05 15:09:28 +0000

    test: introduce ckb-vm-pprof lib

--- crates/generator/src/generator.rs
+++ crates/generator/src/generator.rs
@@ -33,12 +33,9 @@ use gw_types::{
 };
 
 // use gw_types::bytes::Bytes;
-use ckb_vm::{
-    machine::asm::{AsmCoreMachine, AsmMachine},
-    CoreMachine, DefaultMachineBuilder, Machine, Register, SupportMachine,
-};
+use ckb_vm::machine::asm::AsmCoreMachine;
+use ckb_vm_pprof::machine::PProfMachine;
 use core::ops::Deref;
-use std::rc::Rc;
 
 pub struct StateTransitionArgs {
     pub l2block: L2Block,
@@ -461,14 +458,19 @@ impl Generator {
         let mut run_result = RunResult::default();
         // let used_cycles;
         {
-            // let core_machine = AsmCoreMachine::new_with_max_cycles(L2TX_MAX_CYCLES);
+            let account_id = raw_tx.to_id().unpack();
+            let script_hash = state.get_script_hash(account_id)?;
+            let backend = self
+                .load_backend(state, &script_hash)
+                .ok_or(TransactionError::BackendNotFound { script_hash })?;
+
             let core_machine = AsmCoreMachine::new(
                 ckb_vm::ISA_IMC | ckb_vm::ISA_B | ckb_vm::ISA_MOP,
                 ckb_vm::machine::VERSION1,
-                u64::MAX,
+                L2TX_MAX_CYCLES,
             );
-
-            let machine_builder = DefaultMachineBuilder::new(core_machine)
+            let machine_builder = ckb_vm::DefaultMachineBuilder::new(core_machine)
+                .instruction_cycle_func(Box::new(instruction_cycles))
                 .syscall(Box::new(L2Syscalls {
                     chain,
                     state,
@@ -478,43 +480,27 @@ impl Generator {
                     account_lock_manage: &self.account_lock_manage,
                     result: &mut run_result,
                     code_store: state,
-                }))
-                .instruction_cycle_func(Box::new(instruction_cycles));
+                }));
+            let default_machine = machine_builder.build();
 
-            let asm_machine = AsmMachine::new(machine_builder.build(), None);
-            let mut machine = PProfMachine::new(asm_machine);
+            let program = ckb_vm::Bytes::copy_from_slice(backend.generator.deref());
+            let pprof_logger = ckb_vm_pprof::PProfLogger::from_bytes(&program).map_err(|e| {
+                log::error!("error while loading program {:?}: {:?}", program, e);
+                ckb_vm::Error::Unexpected
+            })?;
+            let pprof_func_provider = Box::new(pprof_logger);
 
-            let account_id = raw_tx.to_id().unpack();
-            let script_hash = state.get_script_hash(account_id)?;
-            let backend = self
-                .load_backend(state, &script_hash)
-                .ok_or(TransactionError::BackendNotFound { script_hash })?;
-
+            let mut machine = PProfMachine::new(default_machine, pprof_func_provider);
             // machine.load_program(&backend.generator, &[])?;
-            machine.load_program(
-                &ckb_vm::Bytes::copy_from_slice(backend.generator.deref()),
-                &[],
-            )?;
+            machine.load_program(&program, &[])?;
             let code = machine.run()?;
-
-            // pprof_logger
-            let mut output = std::fs::File::create(format!(
-                "target/pprof{}_{}",
-                nonce_before_execution, account_id
-            ))
-            .expect("can't create file");
-            machine
-                .pprof_logger
-                .unwrap()
-                .tree_root
-                .borrow()
-                .display_flamegraph("", &mut output);
 
             if code != 0 {
                 return Err(TransactionError::InvalidExitCode(code));
             }
             // used_cycles = machine.machine.cycles();
         }
+        // FIXME: method not found in `DefaultMachine<'_, Box<AsmCoreMachine>>`
         // record used cycles
         // run_result.used_cycles = used_cycles;
 
@@ -630,275 +616,4 @@ fn build_challenge_target(
         .target_index(target_index.pack())
         .target_type(target_type.into())
         .build()
-}
-
-// Use tree structure to store ckb vm's runtime data. At present, we care about cycles, but we may add other things in
-// the future, for example, the number of times a certain instruction appears.
-#[derive(Clone, Debug)]
-struct PProfRecordTreeNode {
-    name: String, // FILENAME + FUNCTION_NAME as expected.
-    parent: Option<Rc<std::cell::RefCell<PProfRecordTreeNode>>>,
-    childs: Vec<Rc<std::cell::RefCell<PProfRecordTreeNode>>>,
-    cycles: u64,
-}
-
-impl PProfRecordTreeNode {
-    // Create a new PProfRecordTreeNode with a user defined name(e.g. Function name).
-    fn root() -> Self {
-        Self {
-            name: String::from("??:??"),
-            parent: None,
-            childs: vec![],
-            cycles: 0,
-        }
-    }
-
-    fn display_flamegraph(&self, prefix: &str, writer: &mut impl std::io::Write) {
-        let prefix_name = prefix.to_owned() + self.name.as_str();
-        writer
-            .write_all(format!("{} {}\n", prefix_name, self.cycles).as_bytes())
-            .unwrap();
-        for e in &self.childs {
-            e.borrow()
-                .display_flamegraph(&(prefix_name.as_str().to_owned() + "; "), writer);
-        }
-    }
-}
-
-fn sprint_loc_file(loc: &Option<addr2line::Location>) -> String {
-    if let Some(ref loc) = *loc {
-        let file = loc.file.as_ref().unwrap();
-        let path = std::path::Path::new(file);
-        path.display().to_string()
-    } else {
-        String::from("??")
-    }
-}
-
-fn sprint_fun(
-    mut frame_iter: addr2line::FrameIter<
-        addr2line::gimli::EndianReader<addr2line::gimli::RunTimeEndian, std::rc::Rc<[u8]>>,
-    >,
-) -> String {
-    let mut stack: Vec<String> = vec![String::from("??")];
-    loop {
-        let frame = frame_iter.next().unwrap();
-        if frame.is_none() {
-            break;
-        }
-        let frame = frame.unwrap();
-        let function = frame.function.unwrap();
-        // TODO(clippy::useless_conversion): consider removing `std::borrow::Cow::from()`: `function.raw_name().unwrap()`
-        let function_name = String::from(addr2line::demangle_auto(
-            std::borrow::Cow::from(function.raw_name().unwrap()),
-            function.language,
-        ));
-
-        stack.push(function_name)
-    }
-    stack.last().unwrap().to_string()
-}
-
-// Main handler.
-pub struct PProfLogger {
-    atsl_context: addr2line::Context<
-        addr2line::gimli::EndianReader<addr2line::gimli::RunTimeEndian, std::rc::Rc<[u8]>>,
-    >,
-    tree_root: Rc<std::cell::RefCell<PProfRecordTreeNode>>,
-    tree_node: Rc<std::cell::RefCell<PProfRecordTreeNode>>,
-    ra_dict: std::collections::HashMap<u64, Rc<std::cell::RefCell<PProfRecordTreeNode>>>,
-    // output_filename: String,
-}
-
-impl PProfLogger {
-    pub fn from_path(filename: String) -> Result<Self, Box<dyn std::error::Error>> {
-        let file = std::fs::File::open(filename)?;
-        let mmap = unsafe { memmap::Mmap::map(&file)? };
-        let object = object::File::parse(&*mmap)?;
-        let ctx = addr2line::Context::new(&object)?;
-        let tree_root = Rc::new(std::cell::RefCell::new(PProfRecordTreeNode::root()));
-        Ok(Self {
-            atsl_context: ctx,
-            tree_root: tree_root.clone(),
-            tree_node: tree_root,
-            ra_dict: std::collections::HashMap::new(),
-        })
-    }
-    pub fn from_data(program: &ckb_vm::Bytes) -> Result<Self, Box<dyn std::error::Error>> {
-        let object = object::File::parse(&program)?;
-        let ctx = addr2line::Context::new(&object)?;
-        let tree_root = Rc::new(std::cell::RefCell::new(PProfRecordTreeNode::root()));
-        Ok(Self {
-            atsl_context: ctx,
-            tree_root: tree_root.clone(),
-            tree_node: tree_root,
-            ra_dict: std::collections::HashMap::new(),
-        })
-    }
-}
-
-impl PProfLogger {
-    fn on_step_old<'a>(&mut self, machine: &mut AsmMachine<'a>) {
-        let pc = machine.machine.pc().to_u64();
-        let mut decoder = ckb_vm::decoder::build_decoder::<u64>(machine.machine.isa());
-        let inst = decoder.decode(machine.machine.memory_mut(), pc).unwrap();
-        let inst_length = ckb_vm::instructions::instruction_length(inst) as u64;
-        let opcode = ckb_vm::instructions::extract_opcode(inst);
-        let cycles = machine
-            .machine
-            .instruction_cycle_func()
-            .as_ref()
-            .map(|f| f(inst))
-            .unwrap_or(0);
-        self.tree_node.borrow_mut().cycles += cycles;
-
-        let call = |s: &mut Self, addr: u64, link: u64| {
-            let loc = s.atsl_context.find_location(addr).unwrap();
-            let loc_string = sprint_loc_file(&loc);
-            let frame_iter = s.atsl_context.find_frames(addr).unwrap();
-            let fun_string = sprint_fun(frame_iter);
-            let tag_string = format!("{}:{}", loc_string, fun_string);
-            let chd = Rc::new(std::cell::RefCell::new(PProfRecordTreeNode {
-                name: tag_string,
-                parent: Some(s.tree_node.clone()),
-                childs: vec![],
-                cycles: 0,
-            }));
-            s.tree_node.borrow_mut().childs.push(chd.clone());
-            s.ra_dict.insert(link, s.tree_node.clone());
-            s.tree_node = chd;
-        };
-
-        if opcode == ckb_vm::instructions::insts::OP_JAL {
-            let inst = ckb_vm::instructions::Utype(inst);
-            // The standard software calling convention uses x1 as the return address register and x5 as an alternate
-            // link register.
-            if inst.rd() == ckb_vm::registers::RA || inst.rd() == ckb_vm::registers::T0 {
-                let addr = pc.wrapping_add(inst.immediate_s() as u64) & 0xfffffffffffffffe;
-                let link = pc + inst_length;
-                call(self, addr, link);
-            }
-        };
-        if opcode == ckb_vm::instructions::insts::OP_JALR {
-            let inst = ckb_vm::instructions::Itype(inst);
-            let base = machine.machine.registers()[inst.rs1()].to_u64();
-            let addr = base.wrapping_add(inst.immediate_s() as u64) & 0xfffffffffffffffe;
-            let link = pc + inst_length;
-            if self.ra_dict.contains_key(&addr) {
-                self.tree_node = self.ra_dict.get(&addr).unwrap().clone();
-            } else {
-                call(self, addr, link);
-            }
-        };
-        if opcode == ckb_vm::instructions::insts::OP_FAR_JUMP_ABS {
-            let inst = ckb_vm::instructions::Utype(inst);
-            let addr = (inst.immediate_s() as u64) & 0xfffffffffffffffe;
-            let link = pc + inst_length;
-            if self.ra_dict.contains_key(&addr) {
-                self.tree_node = self.ra_dict.get(&addr).unwrap().clone();
-            } else {
-                call(self, addr, link);
-            }
-        }
-        if opcode == ckb_vm::instructions::insts::OP_FAR_JUMP_REL {
-            let inst = ckb_vm::instructions::Utype(inst);
-            let addr = pc.wrapping_add(inst.immediate_s() as u64) & 0xfffffffffffffffe;
-            let link = pc + inst_length;
-            if self.ra_dict.contains_key(&addr) {
-                self.tree_node = self.ra_dict.get(&addr).unwrap().clone();
-            } else {
-                call(self, addr, link);
-            }
-        }
-    }
-}
-
-/// ckb-vm-pprof-machine
-pub struct PProfMachine<'a> {
-    pub machine: AsmMachine<'a>,
-    pprof_logger: Option<PProfLogger>,
-}
-
-impl CoreMachine for PProfMachine<'_> {
-    type REG = u64;
-    type MEM = Box<AsmCoreMachine>;
-
-    fn pc(&self) -> &Self::REG {
-        &self.machine.machine.pc()
-    }
-
-    fn update_pc(&mut self, pc: Self::REG) {
-        self.machine.machine.update_pc(pc)
-    }
-
-    fn commit_pc(&mut self) {
-        self.machine.machine.commit_pc()
-    }
-
-    fn memory(&self) -> &Self::MEM {
-        self.machine.machine.memory()
-    }
-
-    fn memory_mut(&mut self) -> &mut Self::MEM {
-        self.machine.machine.memory_mut()
-    }
-
-    fn registers(&self) -> &[Self::REG] {
-        self.machine.machine.registers()
-    }
-
-    fn set_register(&mut self, idx: usize, value: Self::REG) {
-        self.machine.machine.set_register(idx, value)
-    }
-
-    fn isa(&self) -> u8 {
-        self.machine.machine.isa()
-    }
-
-    fn version(&self) -> u32 {
-        self.machine.machine.version()
-    }
-}
-
-impl Machine for PProfMachine<'_> {
-    fn ecall(&mut self) -> Result<(), ckb_vm::Error> {
-        self.machine.machine.ecall()
-    }
-
-    fn ebreak(&mut self) -> Result<(), ckb_vm::Error> {
-        self.machine.machine.ebreak()
-    }
-}
-
-impl<'a> PProfMachine<'a> {
-    pub fn new(machine: AsmMachine<'a>) -> Self {
-        Self {
-            machine,
-            pprof_logger: None,
-        }
-    }
-
-    pub fn load_program(
-        &mut self,
-        program: &ckb_vm::Bytes,
-        args: &[ckb_vm::Bytes],
-    ) -> Result<u64, ckb_vm::Error> {
-        self.pprof_logger =
-            Some(PProfLogger::from_data(program).or(Err(ckb_vm::Error::ParseError))?);
-        self.machine.load_program(program, args)
-    }
-
-    pub fn run(&mut self) -> Result<i8, ckb_vm::Error> {
-        let mut decoder = ckb_vm::decoder::build_decoder::<u64>(self.isa());
-        self.machine.machine.set_running(true);
-        if let Some(pprof_logger) = self.pprof_logger.as_mut() {
-            while self.machine.machine.running() {
-                pprof_logger.on_step_old(&mut self.machine);
-                self.machine.step(&mut decoder)?;
-            }
-        } else {
-            unreachable!();
-        }
-        Ok(self.machine.machine.exit_code())
-    }
 }
